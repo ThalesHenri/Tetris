41567b6f7cf3b4eb4ddba33c8cb4fc69c0a30ea5
ThalesHenri <thales1337@hotmail.com>
2022-03-22 17:02:51 -0300

    grid on the screen

--- controlador.py
+++ controlador.py
@@ -11,13 +11,40 @@ run = True
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 RED = 255, 0, 0
-
+GREEN = 0, 255, 0
+font = pygame.font.SysFont('comicsans', 30, True)
 """Screen Definition"""
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tetris")
 
 """Game Objects"""
 clock = pygame.time.Clock()
+
+"""Game functions"""
+
+
+def draw_grid():
+    block = 20
+    for x in range(0, WIDTH, block):
+        for y in range(0, HEIGHT, block):
+            rect = pygame.Rect(x, y, block, block)
+            pygame.draw.rect(SCREEN, WHITE, rect, 1)
+
+
+
+
+def change_form():
+    pass
+
+
+def pop_figure():
+    pass
+
+
+def show_score():
+    text = font.render('Score: ' + str(score), 1, (GREEN))
+    SCREEN.blit(text, (8, 10))
+
 
 """Game loop"""
 while run:
@@ -27,4 +54,6 @@ while run:
             pygame.quit()  # close the game if you click on the x button
     """SCREEN action"""
     SCREEN.fill(BLACK)
+    draw_grid()
+    show_score()
     pygame.display.update()
